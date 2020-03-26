8e1ee29f587fc2a569801ea9cb97dc8e6644d846
darkness666333 <55582596+darkness666333@users.noreply.github.com>
2020-03-26 11:54:34 +0100

    Add files via upload

---
+++ foodgolf/foodgolf/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodgolf
+{
+    class Program
+    {
+        static List<footClass> versenyzok;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            feladat3();
+            feladat4();
+            Console.ReadKey();
+        }
+
+        
+
+        private static void feladat4()
+        {
+            var sz = versenyzok.Count(f => !f.kategoria && f.szazalek == 100);
+            Console.WriteLine($"4.feladat: {sz}");
+        }
+
+        private static void feladat3()
+        {
+            Console.WriteLine($"3.feladat: {versenyzok.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            versenyzok = new List<footClass>();
+            var sr = new StreamReader(@"C:\Users\Merci\source\repos\foodgolf\fob2016.txt", Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new footClass(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ foodgolf/foodgolf/footClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodgolf
+{
+    class footClass
+    {
+        public String nev { get; set; }
+        public bool kategoria{ get; set; }
+        public String egyesulet { get; set; }
+        public int szazalek { get; set; }
+        public int pontok { get; set; }
+        public footClass(string s)
+        {
+            var d = s.Split(';');
+            nev = d[0];
+            kategoria = d[1] == "Felnott ferfi";
+            egyesulet = d[2];
+            pontok = int.Parse(d[3]); int.Parse(d[4]); int.Parse(d[4]);
+            int.Parse(d[5]); int.Parse(d[6]); int.Parse(d[7]); int.Parse(d[8]); int.Parse(d[9]);
+        }
+    }
+}
